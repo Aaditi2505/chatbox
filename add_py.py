f5c26ae3ac0845c0d06c86f22847e35286613bc6
Aaditi2505 <aadititweety2505@gmail.com>
2025-06-17 09:36:20 +0530

    Update add_py.py

--- add_py.py
+++ add_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1TdTpgBxneyMo4aXVZhqlvvqltnuYOXDJ
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
